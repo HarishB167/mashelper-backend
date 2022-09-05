85122538b164a53e45722b795da46c5e8831ead2
Harish <harishpoctor@gmail.com>
2022-09-05 15:19:13 +0530

    Preparing for deployment on Render host.

--- mashelper/settings/prod.py
+++ mashelper/settings/prod.py
@@ -9,3 +9,12 @@ ALLOWED_HOSTS = []
 DATABASES = {
   'default': dj_database_url.config()
 }
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
