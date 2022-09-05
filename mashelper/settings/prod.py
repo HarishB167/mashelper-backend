85ccfea777af80c0aa990a6659fb763e20dda5a3
Harish <harishpoctor@gmail.com>
2022-09-06 03:51:43 +0530

    Reverting additional setting of staticfiles in prod.py

--- mashelper/settings/prod.py
+++ mashelper/settings/prod.py
@@ -16,5 +16,3 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
